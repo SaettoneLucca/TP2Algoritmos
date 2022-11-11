d83e3abe4f05ed2a48f8799fb8eff26326a82b58
SaettoneLucca <lsaettone@fi.uba.ar>
2022-11-11 14:24:30 -0300

    update geolocalizacion

--- prueba geolocalizacion.py
+++ prueba geolocalizacion.py
@@ -9,50 +9,28 @@ def leer_archivo(archivo_multas,datos_csv:list):
         for linea in csv_reader:
             datos_csv.append(linea)
 
-def coordenadas(datos_csv:list):
-    #crea una lista con la latitud y longitud del primer archivo csv 
-    lat_lon:list = []
-    for datos in datos_csv:
-        if datos[1] not in lat_lon:
-            aux:list = []
-            aux.append(datos[2])
-            aux.append(datos[3])
-        lat_lon.append(aux)
-    
-    return lat_lon
 
-def geolocalizador(datos_csv:list):
+def geolocalizador(coordenada):
     #convierte latitud y longitud a una dirreccion
-    direccion:list = []
-    lista:list = coordenadas(datos_csv)
     geolocator = Nominatim(user_agent="multas")
-    for i in lista:
-        ubicacion=geolocator.reverse(i)
-        direccion.append(ubicacion)
-    return print(direccion)
+    ubicacion=geolocator.reverse(coordenada)
+    return ubicacion
 
-    
-def remplazar(datos_csv):
-    pass
-    
-
-
-
-def write_archivo(datos_csv,archivo_2):
-    pass
-    lista = geolocalizador(datos_csv)
-    print(lista)
-
-    with open("csv2.txt","w") as archivo:
-        csv_writer = csv.writer(archivo, delimiter=",", quotechar='"', quoting= csv.QUOTE_NONNUMERIC)
-
+def a(datos_csv):
+    lista_nueva:list = []
+    for dato in datos_csv:
+        aux = []
+        coordenada = dato[2:4]
+        u = (geolocalizador(coordenada)[0])
+        aux.append(u)
+        aux.extend(dato)
+        lista_nueva.append(aux)
+    print(lista_nueva)
 
 def main():
     datos_csv:list =[]
     archivo_multas = open("C:\\Users\\lucca\\OneDrive\\Escritorio\\fiuba\\algoritmos 1\\TP2\\csvtest.txt")
     archivo_2 = open("C:\\Users\\lucca\\OneDrive\\Escritorio\\fiuba\\algoritmos 1\\TP2\\csv2.txt")
     leer_archivo(archivo_multas,datos_csv)
-    coordenadas(datos_csv)
-    geolocalizador(datos_csv)
-    #remplazar(datos_csv)
+    a(datos_csv)
 main()
