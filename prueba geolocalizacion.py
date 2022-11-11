f1fc3fd64b83b7d540969d8cf569d3ad6c8e21e2
SaettoneLucca <lsaettone@fi.uba.ar>
2022-11-10 22:28:11 -0300

    update geolocalizacion

--- prueba geolocalizacion.py
+++ prueba geolocalizacion.py
@@ -1,6 +1,16 @@
+#geolocalizador
+
 from geopy.geocoders import Nominatim 
 
-geolocator = Nominatim()
+def geolocalizador():
+    #convierte latitud y longitud a una dirreccion
+    geolocator = Nominatim(user_agent="multas")
+    ubicacion=geolocator.geocode("Facultad de Ingeniería, Avenida Paseo Colón, Buenos Aires")
+    print(ubicacion.address)
+    print((ubicacion.latitude, ubicacion.longitude))
 
-location = geolocator.geocode("Calle del Universo, 3, Valladolid") 
-print(location.address) 
+def main():
+    
+
+
+main
