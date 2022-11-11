278a99c8bd32393516b99c0d10e9d21c1ac2cc41
SaettoneLucca <lsaettone@fi.uba.ar>
2022-11-11 18:00:45 -0300

    update codigo geolocalizacion

--- prueba geolocalizacion.py
+++ prueba geolocalizacion.py
@@ -14,23 +14,36 @@ def geolocalizador(coordenada):
     #convierte latitud y longitud a una dirreccion
     geolocator = Nominatim(user_agent="multas")
     ubicacion=geolocator.reverse(coordenada)
-    return ubicacion
+    x = str(ubicacion).split(",")
+    y = x[0:4]
 
-def a(datos_csv):
-    lista_nueva:list = []
+    return y
+
+def ubicacion(datos_csv,lista_nueva):
     for dato in datos_csv:
         aux = []
         coordenada = dato[2:4]
-        u = (geolocalizador(coordenada)[0])
-        aux.append(u)
+        u = (geolocalizador(coordenada))#ubicacion
         aux.extend(dato)
+        aux.pop(2)
+        aux.insert(4,u)
+        aux.pop(2)
         lista_nueva.append(aux)
-    print(lista_nueva)
+    return lista_nueva
+
+def write_archivo(lista_nueva,archivo_2):
+    with open("csv2.txt", "w", newline="") as archivo:
+        csv_writer = csv.writer(archivo, delimiter=",", quotechar='"', quoting= csv.QUOTE_NONNUMERIC)
+        for line in lista_nueva:
+            csv_writer.writerow(line)
+
 
 def main():
+    lista_nueva:list = []
     datos_csv:list =[]
     archivo_multas = open("C:\\Users\\lucca\\OneDrive\\Escritorio\\fiuba\\algoritmos 1\\TP2\\csvtest.txt")
     archivo_2 = open("C:\\Users\\lucca\\OneDrive\\Escritorio\\fiuba\\algoritmos 1\\TP2\\csv2.txt")
     leer_archivo(archivo_multas,datos_csv)
-    a(datos_csv)
+    ubicacion(datos_csv,lista_nueva)
+    write_archivo(lista_nueva,archivo_2)
 main()
