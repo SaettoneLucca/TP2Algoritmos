d13e22d8cbf00dca0b5a1786f5825f3b72ed985e
SaettoneLucca <lsaettone@fi.uba.ar>
2022-11-11 12:06:11 -0300

    update

--- tests.py
+++ tests.py
@@ -1,6 +1,5 @@
 #geolocalizador
 import csv
-from geopy.geocoders import Nominatim
 
 
 def leer_archivo(archivo_multas,datos_csv:list):
@@ -10,7 +9,6 @@ def leer_archivo(archivo_multas,datos_csv:list):
             datos_csv.append(linea)
 
 def coordenadas(datos_csv:list):
-    #crea una lista con la latitud y longitud
     lat_lon:list = []
     for datos in datos_csv:
         if datos[1] not in lat_lon:
@@ -19,28 +17,12 @@ def coordenadas(datos_csv:list):
             aux.append(datos[3])
         lat_lon.append(aux)
     
-    return lat_lon
-
-def geolocalizador(datos_csv:list):
-    #convierte latitud y longitud a una dirreccion
-    direccion:list = []
-    lista:list = coordenadas(datos_csv)
-    geolocator = Nominatim(user_agent="multas")
-    for i in lista:
-        ubicacion=geolocator.reverse(i)
-        direccion.append(ubicacion)
-    print(direccion)
-
-    
-def write_archivo(datos_csv):
-    pass
-    lista = geolocalizador(datos_csv)
-
+    print(lat_lon)
 
 def main():
     datos_csv:list =[]
     archivo_multas = open("C:\\Users\\lucca\\OneDrive\\Escritorio\\fiuba\\algoritmos 1\\TP2\\csvtest.txt")
     leer_archivo(archivo_multas,datos_csv)
     coordenadas(datos_csv)
-    geolocalizador(datos_csv)
+
 main()
