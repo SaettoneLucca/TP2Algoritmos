9d664dd7d1826b8f9ebb7ec28f9dfd552daba622
CandelaRiveroA <criveroa@fi.uba.ar>
2022-11-30 10:17:01 -0300

    ya en el codigo principal

--- graph.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt
-names = ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic']
-values = [1, 3, 7, 4, 0, 2, 0, 0, 2, 1, 12, 4]
-
-plt.bar(names, values)
-plt.suptitle('Cant. multas por mes')
-plt.show()
